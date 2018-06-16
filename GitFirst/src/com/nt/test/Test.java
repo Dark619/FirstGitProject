9e825b91555d8424b2565792c4c9f84c2df4a7e0
Dark <Dark@192.168.0.8>
2018-06-16 16:32:30 -0700

    commit

--- GitFirst/src/com/nt/test/Test.java
+++ GitFirst/src/com/nt/test/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		System.out.println("Final Git Hub");
 		System.out.println("DarkShadow is HERE");
 		System.out.println("The Branch is Here");
+		System.out.println("The Branch from developer is Here");
 	}
 
 }
